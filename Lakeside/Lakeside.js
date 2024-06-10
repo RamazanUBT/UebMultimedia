6cfded78fe27b1f36d244b19ff82fd05450d4b69
RamazanUBT <rm57460@ubt-uni.net>
2024-06-10 19:18:30 +0200

    did lakeside and waterfall completely

---
+++ Lakeside/Lakeside.js
@@ -0,0 +1,32 @@
+const audioFiles = [
+    "LakeAudios/LakeSound1.mp3",
+    "LakeAudios/LakeSound2.mp3",
+    "LakeAudios/LakeSound3.mp3",
+  ];
+  
+  let isPlaying = false;
+  let currentAudio = "";
+  
+  function toggleRandomAudio() {
+    const audio = document.getElementById("myAudio");
+    const audioSource = document.getElementById("audioSource");
+    const button = document.getElementById("audioButton");
+  
+    if (isPlaying) {
+      audio.pause();
+      button.textContent = "Play Random Audio";
+      isPlaying = false;
+    } else {
+      const randomIndex = Math.floor(Math.random() * audioFiles.length);
+      const randomAudio = audioFiles[randomIndex];
+      if (currentAudio !== randomAudio) {
+        audioSource.src = randomAudio;
+        currentAudio = randomAudio;
+        audio.load();
+      }
+      audio.play();
+      button.textContent = "Pause Audio";
+      isPlaying = true;
+    }
+  }
+  
